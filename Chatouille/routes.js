1d7d6095e3a9b13386cba44cc64ada9a2a049164
Antoine ROGER <antoine.roger@ingesup.com>
2012-12-12 15:55:33 +0100

    Pages HTML, css, js et routes

---
+++ Chatouille/routes.js
@@ -0,0 +1,21 @@
+﻿var html_path = "/public/";
+
+app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+
+app.use(flatiron.plugins.http, { before : [connect.static("public")] });
+
+
+app.router.get('/', function () {
+    var self = this ;
+    fs.readFile(__dirname + html_path+'index.html','utf-8', function (err, html) {
+        if (err) {
+            self.res.writeHead(404);
+            return self.res.end('index.html introuvable');
+        }
+        self.res.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
+        self.res.end(html,'utf-8');
+
+    });
+});
+
+
