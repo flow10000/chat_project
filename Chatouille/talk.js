1f6fc3f03b73fb5a5be528f622ce78bb7929f36f
Florian <f.fischer10@gmail.com>
2012-12-13 12:23:19 +0100

    Meilleure gestion des caracteres spéciaux pour créer l'url

--- Chatouille/talk.js
+++ Chatouille/talk.js
@@ -6,20 +6,19 @@ Représente une discution archivée
 var Talk = function(messages)
 {
 	this.messages = messages;
-	
-	var firstMessage = messages[0].content;
-	firstMessage = firstMessage.replace(/é|è|ê|ë/g, 'e').replace(/à|â/g, 'a').replace(/î|ï/g, 'i').replace(/ô|ö/g, 'o').replace(/û|ü|ù/g, 'u').replace(/ç/g, 'c');
-	firstMessage = firstMessage.replace(/[^a-zA-Z0-9_' ]/g, '').replace(/'/g, '-');
-	var old = '';
-    while(old != firstMessage)
-    {
-        old = firstMessage;
-        firstMessage = firstMessage.replace(/  /g, ' ');
-    }
-	firstMessage = firstMessage.trim();
-	
-	
-	// name : tag_pseudo_les-premiers-mots-de-la-discution
-	this.name = messages[0].tag + '_' + messages[0].nickName + '_' + firstMessage.replace(/ /g, '-');
 	this.date = new Date();
+	this.name = this.CleanStr(messages[0].tag) + '_' + this.CleanStr(messages[0].nickName) + '_' + this.CleanStr(messages[0].content);
+}
+
+Talk.prototype.CleanStr = function(str)
+{
+	str = str.replace(/é|è|ê|ë/g, 'e').replace(/à|â/g, 'a').replace(/î|ï/g, 'i').replace(/ô|ö/g, 'o').replace(/û|ü|ù/g, 'u').replace(/ç/g, 'c').replace(/[^a-zA-Z0-9_' ]/g, '').replace(/'/g, '-');
+	var old = '';
+	while(old != str)
+    {
+        old = str;
+        str = str.replace(/  /g, ' ');
+    }
+	str = str.trim().replace(/ /g, '-');
+	return str;
 }
