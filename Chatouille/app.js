6ef0c5b14750035442daddd9dcf8304ecb56c05f
Florian <f.fischer10@gmail.com>
2012-12-13 09:18:35 +0100

    Ajout de l'écoute des évenements join et chanquit

--- Chatouille/app.js
+++ Chatouille/app.js
@@ -37,7 +37,8 @@ eval(fs.readFileSync('talk.js', encoding = "utf-8"));
 
 
 var clients = new Array();
-var chatRoom = new ChatRoom();
+var chatRooms = new Array();
+chatRooms['general'] = new ChatRoom('general');
 
 app.start(3000);
 
@@ -46,29 +47,75 @@ var io = require('socket.io').listen(app.server, { log: false });
 io.sockets.on('connection', function(socket)
 {
 	console.log("new client: " + socket.id);
-	clients[socket.id] = new Client(socket);
+	clients[socket.id] = new Client(socket, RdmNickname());
 	
-	// connect client to chatroom
-	chatRoom.NewClient(socket.id, clients[socket.id]);
+	// connect client to chatroom general
+	chatRooms['general'].NewClient(socket.id, clients[socket.id]);
 	
-	socket.on('new_message', function(content)
+	socket.on('new_message', function(data)
 	{
-		chatRoom.NewMessage(socket.id, content);
+		console.log('new_message');
+		console.log(data);
+		//chatRooms['general'].NewMessage(socket.id, data);
+		if(chatRooms[data.tag])
+			chatRooms[data.tag].NewMessage(socket.id, data.content);
+	});
+	
+	socket.on('join', function(chan)
+	{
+		if(!chatRooms[chan])
+			chatRooms[chan] = new ChatRoom(chan);
+
+		chatRooms[chan].NewClient(socket.id, clients[socket.id]);
+	});
+	
+	socket.on('chanquit', function(chan)
+	{
+		if(chatRooms[chan])
+			chatRooms[chan].ClientQuit(socket.id);
 	});
 	
 	socket.on('nickname', function(n)
 	{
-		var old = clients[socket.id].nickName;
-		clients[socket.id].nickName = n;
-		chatRoom.UserChangeNickName(old, n);
+		if(NickNameAvalaible(n))
+		{
+			var old = clients[socket.id].nickName;
+			clients[socket.id].nickName = n;
+			for(var c in chatRooms)
+				chatRooms[c].UserChangeNickName(old, n);
+		}
 	});
 
 	socket.on('disconnect', function()
 	{
 		// disconect client from chatroom
-		chatRoom.ClientDisconnect(socket.id);
+		for(var c in chatRooms)
+			chatRooms[c].ClientDisconnect(socket.id);
 		
 		delete clients[socket.id];
 		clients.splice(socket.id, 1);
 	});
+	
+	
 });
+
+function NickNameAvalaible(nick)
+{
+	if(nick == 'MrChatouille')
+		return false;
+		
+	for(var c in clients)
+		if(clients[c].nickName == nick)
+			return false;
+	
+	return true;
+}
+
+function RdmNickname()
+{
+	var nick;
+	while(!NickNameAvalaible(nick = ('guest' + (Math.floor(Math.random()*11111111)).toString())))
+	{}
+	return nick;
+}
+
