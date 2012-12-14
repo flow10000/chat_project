754f9800e71827d96ccf74ddcf4c58cf47687bc5
Florian <f.fischer10@gmail.com>
2012-12-14 09:24:46 +0100

    gestion des droits, du kick...

--- Chatouille/chatroom.js
+++ Chatouille/chatroom.js
@@ -8,10 +8,12 @@ var ChatRoom = function(tag)
 {
 	this.tag = tag;
 	this.clients = new Array();
+	this.c_count = 0;
 	this.blankTime = 0; // temps en seconde durant lequel personne ne parle
 	this.talk = new Array();
 	this.bot = new Bot(tag, 'MrChatouille');
 	this.tweetMgr = new TweetMgr(this.tag);
+	this.roles = new Array();
 	
 	var _this = this;
 	setInterval(function()
@@ -24,10 +26,12 @@ var ChatRoom = function(tag)
 			if(_this.blankTime > 300) // 5 minutes en conditions réelles
 			{
 				var tlk = new Talk(_this.talk);
-				_this.ArchiveTalk(tlk, function()
+				_this.ArchiveTalk(tlk, function(archive)
 				{
+					delete _this.talk;
 					_this.talk = new Array();
-					_this.bot.Talk("Retrouvez cette discution ici : <a href='http://localhost:3000/discussion/" + tlk.name + "' target='blank'>http://localhost:3000/discussion/" + tlk.name + "</a>", _this.clients);
+					if(archive)
+						_this.bot.Talk("Retrouvez cette discution ici : <a href='http://localhost:3000/discussion/" + tlk.name + "' target='blank'>http://localhost:3000/discussion/" + tlk.name + "</a>", _this.clients);
 				});
 			}
 		}
@@ -49,6 +53,13 @@ var ChatRoom = function(tag)
 ChatRoom.prototype.NewClient = function(id, client)
 {
 	this.clients[id] = client;
+	this.c_count++;
+	
+	if(this.c_count <= 1)
+		this.roles[id] = 'admin';
+	else
+		this.roles[id] = 'user';
+		
 	this.SendConnectedUsers();
 	
 	if(this.talk.length > 0)
@@ -61,40 +72,65 @@ ChatRoom.prototype.DisplayTalk = function(id)
 {
 	for(var t in this.talk)
 		this.clients[id].socket.emit('new_message', this.talk[t]);
-		//this.clients[id].socket.emit('new_message', {tag:this.tag, content:this.talk[t].nickName + ' : ' + this.talk[t].content});
 }
 
 ChatRoom.prototype.ClientQuit = function(id)
 {
 	if(this.clients[id])
-	{
-		var nickname = this.clients[id].nickName;
-		delete this.clients[id];
-		this.clients.splice(id, 1);
-		this.SendConnectedUsers();
-		this.bot.Talk(nickname + " à quitté le salon.", this.clients);
-	}
+		this.ExitClient(id, this.clients[id].nickName + " a quitté le salon.");
 }
 
 ChatRoom.prototype.ClientDisconnect = function(id)
 {
 	if(this.clients[id])
+		this.ExitClient(id, this.clients[id].nickName + " s'est déconnecté.");
+}
+
+ChatRoom.prototype.ExitClient = function(id, message)
+{
+	var nickname = this.clients[id].nickName;
+	var role = this.roles[id];
+	
+	delete this.clients[id];
+	this.clients.splice(id, 1);
+	delete this.roles[id]; 
+	this.roles.splice(id, 1);
+	
+	this.c_count--;
+	if(role == 'admin' && this.c_count > 0)
 	{
-		var nickname = this.clients[id].nickName;
-		if(this.clients[id])
+		// relativement sale mais je ne vois pas comment faire autrement
+		for(var r in this.roles)
 		{
-			delete this.clients[id];
-			this.clients.splice(id, 1);
+			this.roles[r] = 'admin';
+			break;
 		}
-		this.SendConnectedUsers();
-		this.bot.Talk(nickname + " s'est déconnecté.", this.clients);
-	}	
+	}
+	
+	this.SendConnectedUsers();
+	this.bot.Talk(message, this.clients);
 }
 
 ChatRoom.prototype.NewMessage = function(clientId, content)
 {
-	var mess = new Message(this.tag, this.clients[clientId].nickName, content);
-	this.DispatchMessage(mess);
+	if(this.clients[clientId])
+	{
+		if(this.bot.Listen(clientId, new Date()))
+			this.Kick(clientId);
+		else
+			this.DispatchMessage(new Message(this.tag, this.clients[clientId].nickName, content));
+	}
+}
+
+ChatRoom.prototype.Kick = function(id)
+{
+	this.clients[id].socket.emit('kick', this.tag);
+}
+
+ChatRoom.prototype.KickEnd = function(id)
+{
+	if(this.clients[id])
+		this.ExitClient(id, "J'ai kické " + this.clients[id].nickName + " car il floodait. Bien fait pour sa gueule.");
 }
 
 ChatRoom.prototype.NewTweets = function(tweets)
@@ -139,13 +175,40 @@ ChatRoom.prototype.SaveMessage = function(message)
 
 ChatRoom.prototype.ArchiveTalk = function(tlk, cb)
 {
+	if(tlk.messages.length < 10)
+	{
+		cb(false);
+		return;
+	}
+	
+	var messages = tlk.messages;
+	var chaters = new Array();
+	var moreThanOneChater = false;
+	for(var m in messages)
+	{
+		if(messages[m].from == 'user')
+		{
+			if(chaters.length > 0 && chaters[0] != messages[m].nickName)
+			{
+				moreThanOneChater = true;
+				break;
+			}
+			chaters.push(messages[m].nickName);
+		}
+	}
+	if(!moreThanOneChater)	
+	{
+		cb(false);
+		return;
+	}
+	
 	dbMgr.Exec(function()
 	{
 		db.collection('talks', function(err, collection)
 		{
 			collection.insert(tlk, function(err, result)
 			{
-				cb();
+				cb(true);
 			});
 		});
 	});
@@ -154,12 +217,15 @@ ChatRoom.prototype.ArchiveTalk = function(tlk, cb)
 ChatRoom.prototype.SendConnectedUsers = function()
 {
 	var connected_users = new Array();
-	connected_users.push(this.bot.name);
+	
+	connected_users.push({nickname: this.bot.name, role: 'bot'});
 	for(var c in this.clients)
-			connected_users.push(this.clients[c].nickName);
-
+		connected_users.push({nickname: this.clients[c].nickName, role: this.roles[c]});
+		
 	for(var c in this.clients)
 		this.clients[c].socket.emit('connected_users', {tag:this.tag, users:connected_users});
-		
-	
 }
+
+
+
+
